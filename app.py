a8759ef6ffd27942a316a2938545c2829cfee995
Erin Hislope <erinhislope@Erins-MacBook-Pro.local>
2020-08-28 20:44:47 -0700

    updating app.py

--- app.py
+++ app.py
@@ -1,25 +1,82 @@
+# import dependancies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 
+# import Flask
+from flask import Flask, jsonify
+
+#database set up
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+Base.classes.keys()
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# create app and pass __name__
+app = Flask(__name__)
 # Routes
 # /
 # Home page.
+@app.route("/")
+
 # List all routes that are available.
-
-
+# available routes precipitation, stations, tobs, start date and end date (I think)
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<a href='/api/v1.0/precipitation'>Precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>Stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>TOBS</a><br/>"
+        f"<a href='/api/v1.0/temp'>Start/End</a><br/>"
+    )
 
 
 # /api/v1.0/precipitation
+# Convert the query results to a dictionary using date as the key and prcp as the value. - same query as before (last year of data but in a function).
+@app.route("/api/v1.0/precipitation")
+def precipitation(): 
+#     # query last year of data precipitation data
+#     precip_data = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= '2016-08-24').filter\
+#               (Measurement.date <= '2017-08-23').all()
+#     print(precip_data)
+#     precip = []
+#     for date,prcp in precip_data:
+#         precip_dict = {}
+#         precip_dict["date"] = date
+#         precip_dict["precipitation"] = prcp
+#         precip.append(precip_dict) 
+#     return jsonify(precip)
+    
 
 
-# Convert the query results to a dictionary using date as the key and prcp as the value.
-
-
-# Return the JSON representation of your dictionary.
-
-
-
-
-# /api/v1.0/stations
-
+# def precipitation():
+    """Return the precipitation data for the last year"""
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+# Return the JSON representation of your dictionary. - jsonify
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+    
+# /api/v1.0/stations 
 # Return a JSON list of stations from the dataset.
 
 
@@ -27,7 +84,7 @@
 # /api/v1.0/tobs
 
 
-# Query the dates and temperature observations of the most active station for the last year of data.
+# Query the dates and temperature observations of the most active station for the last year of data. - join measurement and Station tables here.
 
 
 # Return a JSON list of temperature observations (TOBS) for the previous year.
@@ -59,3 +116,5 @@
 # Use Flask jsonify to convert your API data into a valid JSON response object.
 
 
+if __name__ == '__main__':
+    app.run(debug=True)
